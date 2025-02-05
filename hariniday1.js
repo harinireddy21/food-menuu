faaf8fd5b2c74ab2c01cd8eab940b46619039142
harinireddy21 <harinireddy299@gmail.com>
2025-02-05 15:41:58 +0530

    initial commit

---
+++ hariniday1.js
@@ -0,0 +1,27 @@
+// type 1
+function name(){
+    console.log('hello world')
+}
+
+// type 2
+const myname = function(){
+    confirm.log('hello world')
+}
+
+// string 
+
+// condition ? exp1 : exp2 ----> ternery operator
+
+(name =="hari") ? console.log('hello world') : console.log('bye har')
+
+//templeate literals 
+let mynam = 'this is said by "hari"';
+
+//sttring interpolation
+let myname = 'this is said by ${name}';
+console.log(myname);
+
+// seq escape 
+
+let display = 'hari';
+console.log(display);
